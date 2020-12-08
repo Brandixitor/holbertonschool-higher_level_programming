9f305f9e6eeac711afdcd095ff476a2f65705a30
Brandixitor <med.salah.bourouba@gmail.com>
2020-12-08 22:08:24 +0100

    Tried to fix a couple of issues

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if (number < 0):
-        number = number % - 10
-        print(-number, end="")
-        return (-number)
+    if number < 0:
+        print("{}".format(-number % 10), end='')
+        return -number % 10
     else:
-        number = number % 10
-        print(number, end="")
-return (number)
+        print("{}".format(number % 10), end='')
+return number % 10
