c984803d2c827bae3fc266cbbf0bf2d12066d395
Brandixitor <med.salah.bourouba@gmail.com>
2021-03-02 17:28:55 +0100

    Fixed

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
 -- SQL script that creates a table.
-CREATE TABLE IF NOT EXISTS unique_id {
+CREATE TABLE IF NOT EXISTS unique_id (
     id INT DEFAULT 1,
-    name VARCHAR(256);
-}
+    name VARCHAR(256)
+    );
