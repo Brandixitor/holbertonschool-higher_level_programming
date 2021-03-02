7a8bd30a0ee712031d01eda72b74fda84bb21625
Brandixitor <med.salah.bourouba@gmail.com>
2021-03-02 17:41:37 +0100

    First commit

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- SQL script that creates a database and a user.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS user_0d_2@localhost 
+IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtnh_0d_2 TO user_0d_2@localhost;
