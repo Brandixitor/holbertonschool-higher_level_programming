32fe07bcae1a4bdbff2483a9687249806f7e324b
Brandixitor <med.salah.bourouba@gmail.com>
2021-03-02 17:56:53 +0100

    Fixed

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- SQL script that creates a database and a user.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtnh_0d_2 TO user_0d_2@localhost;
+GRANT SELECT ON hbtnh_0d_2.* TO user_0d_2@localhost;
