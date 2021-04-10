0f00f9bb825c4a6b58b94e19dd83f2368f0ab4a2
Brandixitor <med.salah.bourouba@gmail.com>
2021-04-10 13:17:36 +0100

    first commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""""A Python script that fetches for https://intranet.hbtn.io/status""""
+from urllib import request
+
+if __name__ == "__main__":
+    with request.urlopen("https://intranet.hbtn.io/status") as page:
+        content = page.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
